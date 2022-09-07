93b04ddeede812675726b4eb203793158d844e4e
Simon Cockx <simon@sikanda.be>
2022-09-08 00:01:39 +0200

    Initial Ionic+Nest setup

---
+++ apps/my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ionic-nestjs-test-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
