e64100320fcdcd87ed25d393e5fd8645f8e962d2
Simon Cockx <simon@sikanda.be>
2022-11-21 22:49:03 +0100

    Stuff

--- apps/my-api/src/app/app.module.ts
+++ apps/my-api/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      ServeStaticModule.forRoot({
+        rootPath: join(process.cwd(), 'dist', 'apps', 'my-app'),
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
